d7d68238d25242e8a993e4b591a6f68bd760f7f0
Mark Hammond <mhammond@skippinet.com.au>
2011-07-25 14:55:49 +1000

    drop services.share.shareURL preference.

--- services-share.js
+++ services-share.js
@@ -1,3 +1,1 @@
-pref("services.share.shareURL", "https://f1.mozillamessaging.com/share/panel/");
-pref("services.share.settingsURL", "https://f1.mozillamessaging.com/share/settings/");
 pref("services.share.bookmarking", true);
